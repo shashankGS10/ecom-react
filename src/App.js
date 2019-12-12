dabc08c1a92af6fbac333c2020be5aad0d53d342
shashankGS10 <gsshashank@gmail.com>
2019-12-12 12:00:18 +0530

    update menu-item and directory

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HomePage from './pages/homepage/homepage.component.jsx';
+import { Route, Switch, Link } from 'react-router-dom';
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>HAts Page</h1>
+      <Link to={`${props.match.url}/12`}> hat id : 12</Link>
+    </div>
+  )
+}
+
+// const JacketsPage = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <button onClick={() => props.history.push('/jackets')}>History</button>
+//       <h1>jackets mOfos</h1>
+//     </div>
+//   )
+// }
+// const SnikersPage = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>snikers mOfos</h1>
+//     </div>
+//   )
+// }
+// const MensPage = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>mens collection mOfos</h1>
+//     </div>
+//   )
+// }
+
+// const WomensPage = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>womens collection mOfos : {props.match.params.productId}</h1>
+//     </div>
+//   )
+// }
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+        {/* <Route path='/jackets' component={JacketsPage} />
+        <Route path='/snikers' component={SnikersPage} />
+        <Route path='/mens' component={MensPage} />
+        <Route exact path='/womens' component={WomensPage} />
+        <Route path='/womens/:productId' component={WomensPage} /> */}
+      </Switch>
+
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './homepage.style.scss';
+import Directory from '../../components/directory-menu/directory-menu.component'
+
+export default function HomePage() {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
